c9fcd3f2c62d3a764faa8eb1380c1ef4858d4b3c
huybui2305 <huybui2305@gmail.com>
2026-03-04 14:19:39 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = { reactStrictMode: true }
-=======
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
->>>>>>> 4d2356d0fb5fd286f507361ae208200f3b380b43
 module.exports = nextConfig
